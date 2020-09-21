6a6f70d3fb47eb2f224632fad4eab58a86a95014
King.Zevin <king.zevin@gmail.com>
2020-09-21 17:40:47 +0800

    fix(serverless-documents-service-api): added the "@Body" to the policyRegisteredNotify method

--- serverless-documents-service-api/src/main/kotlin/pl/altkom/asc/lab/micronaut/poc/serverless/documents/api/DocumentsOperations.kt
+++ serverless-documents-service-api/src/main/kotlin/pl/altkom/asc/lab/micronaut/poc/serverless/documents/api/DocumentsOperations.kt
@@ -5,6 +5,7 @@ import pl.altkom.asc.lab.micronaut.poc.policy.service.api.v1.events.PolicyRegist
 
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Body
 import org.jetbrains.annotations.NotNull
 
 interface DocumentsOperations {
@@ -13,5 +14,5 @@ interface DocumentsOperations {
     fun find(@NotNull policyNumber: String): FindDocumentsResult
 
     @Post("/register")
-    fun policyRegisteredNotify(@NotNull event: PolicyRegisteredEvent)
+    fun policyRegisteredNotify(@NotNull @Body event: PolicyRegisteredEvent)
 }
